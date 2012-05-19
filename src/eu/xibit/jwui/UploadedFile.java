39e4b369964ca16a3bdef157aea41453f14fc61e
David Ipacs <david.ipacs@gmail.com>
2012-05-19 18:23:04 +0200

    JavaDoc Documentation

--- src/eu/xibit/jwui/UploadedFile.java
+++ src/eu/xibit/jwui/UploadedFile.java
@@ -5,6 +5,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import org.apache.commons.fileupload.FileItem;
 
+/**
+ * This class holds some informations from an uploaded file.
+ * 
+ * @author dipacs
+ */
 public final class UploadedFile {
     
     private final FileItem fileItem;
@@ -18,35 +23,85 @@ public final class UploadedFile {
 
     
     
-    
-    public void write(File file) throws Exception {
+    /**
+     * Copy the uploaded file to the given file.
+     * 
+     * @param file
+     * The target file.
+     * 
+     * @throws Exception 
+     * If the copy isn't success.
+     */
+    public void copy(File file) throws Exception {
         fileItem.write(file);
     }
 
+    /**
+     * Returns the size of the file in bytes.
+     * 
+     * @return 
+     * The size of the file in bytes.
+     */
     public long getSize() {
         return fileItem.getSize();
     }
 
+    /**
+     * Returns the name of the file.
+     * 
+     * @return 
+     * The name of the file.
+     */
     public String getName() {
         return fileItem.getName();
     }
 
+    /**
+     * Returns the input stream which can be used to read the content of the file.
+     * 
+     * @return
+     * the input stream which can be used to read the content of the file.
+     * 
+     * @throws IOException 
+     * If something is went wrong.
+     */
     public InputStream getInputStream() throws IOException {
         return fileItem.getInputStream();
     }
 
+    /**
+     * Returns the name of the html input tag which uploads this file.
+     * 
+     * @return 
+     * the name of the html input tag which uploads this file.
+     */
     public String getFieldName() {
         return fileItem.getFieldName();
     }
 
+    /**
+     * Returns the content type of the file eg.: text/html.
+     * 
+     * @return 
+     * the content type of the file eg.: text/html.
+     */
     public String getContentType() {
         return fileItem.getContentType();
     }
 
-    public byte[] get() {
+    /**
+     * Returns all the bytes of the file's content as a byte array.
+     * 
+     * @return 
+     * all the bytes of the file's content as a byte array.
+     */
+    public byte[] getBytes() {
         return fileItem.get();
     }
 
+    /**
+     * Deletes this file from the temporary storage.
+     */
     public void delete() {
         fileItem.delete();
     }
--- src/eu/xibit/jwui/components/AComponent.java
+++ src/eu/xibit/jwui/components/AComponent.java
@@ -1,36 +1,43 @@
 package eu.xibit.jwui.components;
 
+/**
+ * This class is the base class of all the JWUI components.
+ * You can inherite from this class if you want to create a custom component.
+ * <p>
+ * Note that this is a low-level way of creating components. This way is useful if you want to create a totally new 
+ * component, which can't be created as compositing already exists components. If you just want to composit already exists components to create your own, 
+ * use <code>AUserControl</code>.
+ * </p>
+ * 
+ * @author dipacs
+ */
 public abstract class AComponent {
 
     private static int ActId = 1;
 
-    public static String getNextId() {
+    private static String getNextId() {
         String res = "id" + Integer.toString(AComponent.ActId);
         AComponent.ActId = AComponent.ActId + 1;
         return res;
     }
 
+    
+    
+    
     private String id;
     private double width = -1.0;
     private double height = -1.0;
     private boolean visible = true;
     private EHAlign horizontalAlign = EHAlign.Left;
     private EVAlign verticalAlign = EVAlign.Top;
-    private String onKeyDownScript = null;
-    private String onKeyPressScript = null;
-    private String onKeyUpScript = null;
-    private String onClickScript = null;
-    private String onDoubleClickScript = null;
-    private String onMouseDownScript = null;
-    private String onMouseMoveScript = null;
-    private String onMouseUpScript = null;
-    private String onMouseOutScript = null;
-    private String onMouseOverScript = null;
 
 
 
 
-    public AComponent() {
+    /**
+     * Creates a new instance of AComponent, and generates a new id for it.
+     */
+    protected AComponent() {
         super();
         this.id = AComponent.getNextId();
     }
@@ -38,49 +45,151 @@ public abstract class AComponent {
 
 
 
+    /**
+     * Returns the id of this component. This id can be used from JavaScript to access this component.
+     * 
+     * @return 
+     * The id of this component.
+     */
     public final String getId() {
         return this.id;
     }
 
+    /**
+     * Sets the id of this component to the given string. This id can be used from JavaScript to access this component.
+     * 
+     * @param id 
+     * The new id of this component.
+     */
     public final void setId(String id) {
         this.id = id;
     }
 
     
+    /**
+     * Returns the width of the component.
+     * <p>
+     * This is a multi function number. See the following table to reference:
+     * <ul>
+     * <li>width &lt; 0 --> undefined (autosize)</li>
+     * <li>width = 0 --> 0 pixel</li>
+     * <li>0 &lt; width &lt; 1 --> percent</li>
+     * <li>width &gt; 1 --> pixels</li>
+     * </ul>
+     * </p>
+     * 
+     * @return The width of the component.
+     */
     protected double getWidth() {
         return this.width;
     }
 
+    /**
+     * Sets the width of this component.
+     * <p>
+     * This is a multi function number. See the following table to reference:
+     * <ul>
+     * <li>width &lt; 0 --> undefined (autosize)</li>
+     * <li>width = 0 --> 0 pixel</li>
+     * <li>0 &lt; width &lt; 1 --> percent</li>
+     * <li>width &gt; 1 --> pixels</li>
+     * </ul>
+     * </p>
+     * 
+     * @param width 
+     * The new width of the component.
+     */
     protected void setWidth(double width) {
         this.width = width;
     }
 
+    /**
+     * Returns the height of this component.
+     * <p>
+     * This is a multi function number. See the following table to reference:
+     * <ul>
+     * <li>width &lt; 0 --> undefined (autosize)</li>
+     * <li>width = 0 --> 0 pixel</li>
+     * <li>0 &lt; width &lt; 1 --> percent</li>
+     * <li>width &gt; 1 --> pixels</li>
+     * </ul>
+     * </p>
+     * 
+     * @return 
+     * The height of this component.
+     */
     protected double getHeight() {
         return this.height;
     }
 
+    /**
+     * Sets the height of this component.
+     * <p>
+     * This is a multi function number. See the following table to reference:
+     * <ul>
+     * <li>width &lt; 0 --> undefined (autosize)</li>
+     * <li>width = 0 --> 0 pixel</li>
+     * <li>0 &lt; width &lt; 1 --> percent</li>
+     * <li>width &gt; 1 --> pixels</li>
+     * </ul>
+     * </p>
+     * 
+     * @param height 
+     * The new height of this component.
+     */
     protected void setHeight(double height) {
         this.height = height;
     }
 
+    /**
+     * Returns the horizontal alignment of this component.
+     * 
+     * @return 
+     * The horizontal alignment of this component.
+     */
     public final EHAlign getHorizontalAlign() {
         return this.horizontalAlign;
     }
 
+    /**
+     * Sets the horizontal alignment of this component.
+     * 
+     * @param horizontalAlign 
+     * The new horizontal alignemt of this component.
+     */
     public final void setHorizontalAlign(EHAlign horizontalAlign) {
         this.horizontalAlign = horizontalAlign;
     }
 
+    /**
+     * Returns the vertical alignment of this component.
+     * 
+     * @return 
+     * The vertical alignment of this component.
+     */
     public final EVAlign getVerticalAlign() {
         return this.verticalAlign;
     }
 
+    /**
+     * Sets the vertical alignment of this component.
+     * 
+     * @param verticalAlign 
+     * The new vertical alignemt of this component.
+     */
     public final void setVerticalAlign(EVAlign verticalAlign) {
         this.verticalAlign = verticalAlign;
     }
 
 
 
+    /**
+     * Returns the css style which contains the style of this component, and which can be inserted in the style parameter
+     * of the html tag.
+     * 
+     * @return 
+     * The css style which contains the style of this component as String.
+     */
     protected final String renderSizeStyle() {
         String res = "";
 
@@ -123,16 +232,37 @@ public abstract class AComponent {
         return res;
     }
 
+    /**
+     * Indicates if this component is visible or not.
+     * 
+     * @return 
+     * True if this component is visible, otherwise false.
+     */
     public final boolean isVisible() {
         return this.visible;
     }
 
+    /**
+     * Sets the visible property of this component.
+     * 
+     * @param value 
+     * The new value of the visible property.
+     */
     public final void setVisible(boolean value) {
         this.visible = value;
     }
 
 
 
+    /**
+     * This method is invoked by the JWUI core when the resulting html is built.
+     * <p>
+     * You need to overwrite this method, to return as String which represents your control in HTML. The resulting HTML
+     * will be inserted to the final HTML file.
+     * </p>
+     * 
+     * @return The HTML String which fully represents this control.
+     */
     public abstract String render();
 
 }
