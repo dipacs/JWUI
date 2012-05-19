ccaedd82783c19c410c38c1c5948851b83bf77c6
David Ipacs <david.ipacs@gmail.com>
2012-05-19 18:23:27 +0200

    Base implementation of Response class

--- src/eu/xibit/jwui/Response.java
+++ src/eu/xibit/jwui/Response.java
@@ -4,6 +4,8 @@
  */
 package eu.xibit.jwui;
 
+import java.io.IOException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
 /**
@@ -14,8 +16,54 @@ public class Response {
     
     private final HttpServletResponse response;
 
+    
+    
+    
     Response(HttpServletResponse response) {
         this.response = response;
+    }
+
+    
+    
+    
+    public void setContentType(String type) {
+        response.setContentType(type);
+    }
+
+    public void setCharacterEncoding(String charset) {
+        response.setCharacterEncoding(charset);
+    }
+
+    public String getContentType() {
+        return response.getContentType();
+    }
+
+    public String getCharacterEncoding() {
+        return response.getCharacterEncoding();
+    }
+
+    public void setStatus(int sc) {
+        response.setStatus(sc);
+    }
+
+    public void sendRedirect(String location) throws IOException {
+        response.sendRedirect(location);
+    }
+
+    public void sendError(int sc) throws IOException {
+        response.sendError(sc);
+    }
+
+    public void sendError(int sc, String msg) throws IOException {
+        response.sendError(sc, msg);
+    }
+
+    public void addHeader(String name, String value) {
+        response.addHeader(name, value);
+    }
+
+    public void addCookie(Cookie cookie) {
+        response.addCookie(cookie);
     }
     
 }
