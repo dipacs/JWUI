d1046952f1052f29633817cdcc007086edb7ac08
David Ipacs <david.ipacs@gmail.com>
2012-05-20 13:56:30 +0200

    The javadoc documentation of the Link class is added.

--- src/eu/xibit/jwui/Link.java
+++ src/eu/xibit/jwui/Link.java
@@ -1,5 +1,10 @@
 package eu.xibit.jwui;
 
+/**
+ * This class represents a target of a link. You can define the target url, and the typo of opening the link.
+ * 
+ * @author dipacs
+ */
 public final class Link {
 
     private String url; 
@@ -8,6 +13,10 @@ public final class Link {
 
 
 
+	/**
+	 * Creates a new instance of <code>Link</code>.
+	 * @param url 
+	 */
     public Link(String url) {
         super();
         this.url = url;
@@ -16,20 +25,42 @@ public final class Link {
 
 
 
+	/**
+	 * Returns the target URL of this link.
+	 * 
+	 * @return 
+	 * The target URL of this link.
+	 */
     public String getUrl() {
         return this.url;
     }
 
+	/**
+	 * Indicates if this link needs to be opened in a new browser tab.
+	 * 
+	 * @return 
+	 * True if this link must be opened in a new browser tab, otherwise false.
+	 */
     public boolean isInNewTab() {
         return this.inNewTab;
     }
 
+	/**
+	 * Sets the new tab property of this link.
+	 * 
+	 * @param value 
+	 * The new value of the in new tab property.
+	 */
     public void setInNewTab(boolean value) {    
         this.inNewTab = value;
     }
 
 
 
+	/**
+	 * 
+	 * @return 
+	 */
     @Override
     public String toString() {
         return this.url;
